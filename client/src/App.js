140cf534b6731ea5430511e42c69e522f6359c8f
DONGVAN <viendong194@gmail.com>
2018-06-02 07:42:21 +0900

    eject react

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import styled from 'styled-components';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <BrowserRouter basename="/">
+          <Center>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+            <Footer />
+          </Center>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
+
+const Center = styled.div`
+  display:block;
+  margin:0 auto;
+  width:100%;
+  height:100vh;
+  background-color:black;
+`;
